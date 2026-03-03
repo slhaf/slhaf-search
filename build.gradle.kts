c356a0e0d20d169163c2d9279d0184c6d45719ef
slhafzjw <slhafzjw@slhaf.work>
2026-03-03 22:29:49 +0800

    Initialize Ktor project scaffold with routing, JSON serialization, and Gradle setup

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    kotlin("jvm") version "2.3.0"
+    id("io.ktor.plugin") version "3.4.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0"
+}
+
+group = "work.slhaf"
+version = "0.0.1"
+
+application {
+    mainClass = "work.slhaf.ApplicationKt"
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-serialization-kotlinx-json")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
